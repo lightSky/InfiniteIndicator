0aeb786c2065fda035cbe14bd07cc8f02ddff923
lightSky <595806797@qq.com>
2015-08-11 18:26:06 +0800

    fix handler memory leak

--- library/src/main/java/cn/lightsky/infiniteindicator/InfiniteIndicatorLayout.java
+++ library/src/main/java/cn/lightsky/infiniteindicator/InfiniteIndicatorLayout.java
@@ -13,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.animation.Interpolator;
 import android.widget.RelativeLayout;
 
+import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 
 import cn.lightsky.infiniteindicator.indicator.PageIndicator;
@@ -26,6 +27,7 @@ import cn.lightsky.infiniteindicator.slideview.BaseSliderView;
  * Thanks to: https://github.com/Trinea/android-auto-scroll-view-pager
  */
 public class InfiniteIndicatorLayout extends RelativeLayout implements RecyclingPagerAdapter.DataChangeListener {
+    private final ScrollHandler handler;
     private PageIndicator mIndicator;
     private ViewPager mViewPager;
     private Context mContext;
@@ -113,6 +115,7 @@ public class InfiniteIndicatorLayout extends RelativeLayout implements Recycling
         else
             LayoutInflater.from(context).inflate(R.layout.layout_anim_line_indicator, this, true);
 
+        handler = new ScrollHandler(this);
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
         mRecyleAdapter = new RecyleAdapter(mContext);
         mRecyleAdapter.setDataChangeListener(this);
@@ -178,6 +181,11 @@ public class InfiniteIndicatorLayout extends RelativeLayout implements Recycling
         /** remove messages before, keeps one message is running at most **/
         handler.removeMessages(MSG_WHAT);
         handler.sendEmptyMessageDelayed(MSG_WHAT, delayTimeInMills);
+    }
+
+    private void sendScrollMessage() {
+        /** remove messages before, keeps one message is running at most **/
+        sendScrollMessage(interval);
     }
 
     /**
@@ -270,19 +278,30 @@ public class InfiniteIndicatorLayout extends RelativeLayout implements Recycling
             mIndicator.notifyDataSetChanged();
     }
 
-    private Handler handler = new Handler(){
+    public static class ScrollHandler extends Handler{
+        public WeakReference<InfiniteIndicatorLayout> mLeakActivityRef;
+
+        public ScrollHandler(InfiniteIndicatorLayout infiniteIndicatorLayout) {
+            mLeakActivityRef = new WeakReference<InfiniteIndicatorLayout>(infiniteIndicatorLayout);
+        }
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch (msg.what) {
-                case MSG_WHAT:
-                    scrollOnce();
-                    sendScrollMessage(interval);
-                default:
-                    break;
+
+            InfiniteIndicatorLayout infiniteIndicatorLayout = mLeakActivityRef.get();
+            if(infiniteIndicatorLayout != null){
+                switch (msg.what) {
+                    case MSG_WHAT:
+                        infiniteIndicatorLayout.scrollOnce();
+                        infiniteIndicatorLayout.sendScrollMessage();
+                    default:
+                        break;
+                }
             }
         }
-    };
+    }
+
 
     /**
      * get auto scroll interval time in milliseconds, default is {@link #DEFAULT_INTERVAL}
@@ -428,4 +447,9 @@ public class InfiniteIndicatorLayout extends RelativeLayout implements Recycling
             mIndicator.setOnPageChangeListener(onPageChangeListener);
     }
 
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        handler.removeCallbacksAndMessages(null);
+    }
 }
